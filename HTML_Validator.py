ad13426ab16e5bef20ad4bca69a224f3f8763983
selina-28030 <54951726+selina-28030@users.noreply.github.com>
2020-02-12 21:58:18 -0800

    Update HTML_Validator.py

--- HTML_Validator.py
+++ HTML_Validator.py
@@ -19,42 +19,42 @@ def validate_html(html):
 		return TRUE
 	else:
 		return FALSE
-    '''
-    This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
+    	'''
+    	This function performs a limited version of html validation by checking whether every opening tag has a corresponding closing tag.
 
-    >>> validate_html('<strong>example</strong>')
-    True
-    >>> validate_html('<strong>example')
-    False
-    '''
+    	>>> validate_html('<strong>example</strong>')
+    	True
+    	>>> validate_html('<strong>example')
+    	False
+    	'''
 
-    # HINT:
-    # use the _extract_tags function below to generate a list of html tags without any extra text;
-    # then process these html tags using the balanced parentheses algorithm from the book
-    # the main difference between your code and the book's code will be that you will have to keep track of not just the 3 types of parentheses,
-    # but arbitrary text located between the html tags
+    	# HINT:
+    	# use the _extract_tags function below to generate a list of html tags without any extra text;
+    	# then process these html tags using the balanced parentheses algorithm from the book
+    	# the main difference between your code and the book's code will be that you will have to keep track of not just the 3 types of parentheses,
+    	# but arbitrary text located between the html tags
 
 
 def get_first_tag(html):
-    start = html.index('<')
-    end = html.index('>')+1
-    tag = html[start:end]
-    return tag
+	start = html.index('<')
+    	end = html.index('>')+1
+    	tag = html[start:end]
+    	return tag
 
 
 def _extract_tags(html):
-    if '<' not in html:
-        return []
-    else:
-        return [get_first_tag(html)]+get_tags(html[html.index('>')+1:])
+    	if '<' not in html:
+        	return []
+    	else:
+        	return [get_first_tag(html)]+get_tags(html[html.index('>')+1:])
     
-    '''
-    This is a helper function for `validate_html`.
-    By convention in Python, helper functions that are not meant to be used directly by the user are prefixed with an underscore.
+    	'''
+    	This is a helper function for `validate_html`.
+    	By convention in Python, helper functions that are not meant to be used directly by the user are prefixed with an underscore.
 
-    This function returns a list of all the html tags contained in the input string,
-    stripping out all text not contained within angle brackets.
+    	This function returns a list of all the html tags contained in the input string,
+    	stripping out all text not contained within angle brackets.
 
-    >>> _extract_tags('Python <strong>rocks</strong>!')
-    ['<strong>', '</strong>']
-    '''
+    	>>> _extract_tags('Python <strong>rocks</strong>!')
+    	['<strong>', '</strong>']
+    	'''
